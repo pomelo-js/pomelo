90470fb37c19df1b4ae67b05ddfc063deafab63b
YuzuTea <flin00806@gmail.com>
2025-02-19 22:25:46 +0800

    fix: 解决了execa调用不当的问题

--- src/utils/shell.ts
+++ src/utils/shell.ts
@@ -1,10 +1,12 @@
-import { $ } from "execa";
+import { execa } from "execa";
 
 /**
  * 执行shell命令
  * @param command 整句命令
  */
 export async function carryCommand(command: string) {
-    const { stdout } = await $`${command}`;
+    const trunks = command.split(" ");
+    const base = trunks.shift() || "";
+    const { stdout } = await execa(base, trunks);
     console.log(stdout);
 }
